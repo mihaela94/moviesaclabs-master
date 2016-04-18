67a8588a8553b126f471dbb2ce7a338b41c699a5
mihaela94 <mihaela.ilin@yahoo.com>
2016-04-18 16:47:45 +0300

    lab4
    lab4

---
+++ MoviesACLabs/Scripts/lab4.js
@@ -0,0 +1,25 @@
+﻿var app = angular.module('todo', []);
+
+app.controller('todoCtrl', function ($scope) {
+    //initialize empty list of todo items
+    $scope.list = [];
+    $scope.newItem;
+
+    $scope.finishedTasks = [];
+
+    $scope.addTaskEnter = function (event) {
+        if (event.keyCode == 13) {
+            $scope.addTask();
+        }
+    }
+
+    //Itemul meu ar trebui sa fie un obiect, care are un nume si o valoare de "checked".
+    $scope.addTask = function () {
+        $scope.list[$scope.list.length] = $scope.newItem;
+        $scope.newItem = '';
+    }
+
+    $scope.check = function (task) {
+        $scope.finishedTasks[$scope.finishedTasks.length] = task;
+    }
+});
